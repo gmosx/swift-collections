de0c9c7cb4a029f10fc5da15f9e4048359857bde
George Moschovitis <gmosx@reizu.com>
2018-06-13 18:54:05 +0300

    Removed Graph

--- Sources/Collections/Deque.swift
+++ Sources/Collections/Deque.swift
@@ -1,6 +1,13 @@
 /// https://en.wikipedia.org/wiki/Double-ended_queue
 
+// TODO: consider extending Queue?
 // kind-of combines Queue + Stack (FIFO, LIFO)
 
+/// A Double-Ended Queue (Deque) is a collection that generalizes a queue, for
+/// which elements can be added to or removed from either the front (head) or
+/// back (tail).
+///
+/// https://en.wikipedia.org/wiki/Double-ended_queue
 public protocol Deque {
+    associatedtype Element
 }
--- Sources/Collections/Queue.swift
+++ Sources/Collections/Queue.swift
@@ -1,14 +1,18 @@
 
+// FIFO
+
 // TODO: consider renaming to PriorityQueue? i.e. FIFO
 
-/// This protocol is as general as possible.
+/// A queue is a collection in which elements are kept in order and the only
+/// operations are the addition of elements to the rear terminal position and
+/// removal of elements from the front terminal position.
+///
+/// General names like `enqueue` and `dequeue` are used to make this protocol
+/// as abstract as possible.
 public protocol Queue {
     associatedtype Element
 
-    func enqueue(_ element: Element)
-    func dequeue() -> Element
-
-//    func append(_ element: Element)
-//    func removeFirst() -> Element
+    func enqueue(_ element: Element) // i.e. append()
+    func dequeue() -> Element // i.e removeFirst()
 }
 
